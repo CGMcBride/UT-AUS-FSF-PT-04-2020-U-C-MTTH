a147cacbcc9270f10f569ca84f32bf8f11cd2aeb
Antonio Gage <antoniojgage@gmail.com>
2020-05-07 20:37:39 -0500

    up to 27 solved

--- 01-Class-Content/04-Web-APIs/01-Activities/20-Ins_Local-Storage-Counter/script.js
+++ 01-Class-Content/04-Web-APIs/01-Activities/20-Ins_Local-Storage-Counter/script.js
@@ -2,18 +2,21 @@ var counter = document.querySelector("#counter");
 var addButton = document.querySelector("#add");
 var subtractButton = document.querySelector("#subtract");
 
-var count = localStorage.getItem("count");
-
+var count = 0;
 counter.textContent = count;
+if (localStorage.getItem("count")) {
+  count = localStorage.getItem("count");
+  counter.textContent = count;
+}
 
-addButton.addEventListener("click", function() {
+addButton.addEventListener("click", function () {
   count++;
   counter.textContent = count;
 
   localStorage.setItem("count", count);
 });
 
-subtractButton.addEventListener("click", function() {
+subtractButton.addEventListener("click", function () {
   count--;
   counter.textContent = count;
 

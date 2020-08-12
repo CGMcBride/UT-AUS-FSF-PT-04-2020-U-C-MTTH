bb250d2fe09e7de4b4f3219ecaac517061a94f1e
CGMcBride <connermcbride@ymail.com>
2020-08-11 21:33:04 -0500

    adding my solution

--- 01-Class-Content/17-NoSQL/01-Activities/06-Stu-MongoJS-Sorting/Unsolved/server.js
+++ 01-Class-Content/17-NoSQL/01-Activities/06-Stu-MongoJS-Sorting/Unsolved/server.js
@@ -29,7 +29,15 @@ app.get("/all", (req, res) => {
 // TODO: Implement the remaining two routes
 
 // 1: Name: Send JSON response sorted by name in ascending order, e.g. GET "/name"
-
+app.post("/all", (req, res) => {
+  db.animals.find().sort({ name: 1 }, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(data);
+    }
+  });
+});
 // 2: Weight: Send JSON response sorted by weight in descending order, , e.g. GET "/weight"
 
 // Set the app to listen on port 3000
